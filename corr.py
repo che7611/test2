581867e57399f0c99a40fdc0ac22ccf8b64b57e2
che7611 <39072982@qq.com>
2019-04-17 23:44:38 +0800

    corr

--- corr.py
+++ corr.py
@@ -7,16 +7,21 @@ import datetime as dt
 from matplotlib.lines import TICKLEFT, TICKRIGHT, Line2D
 from matplotlib.patches import Rectangle
 import datetime as dt
+import mpl_toolkits.axisartist as AA
 
 class Analyse_CORR:
-    def __init__(self,source,target):
-        self.Data=source #数据源
-        self.DataT=target
-        self.Amount=len(target)  #分析的个数
-        self.V1=target['close'].values #分析的数据集
-        self.V2=source['close'].values #总数据集
-        self.V2_LEN=len(self.V2)
-        self.fig = plt.figure(figsize=(30, 12))
+    def __init__(self,source=None,target=None):
+        if source is not None and target is not None:
+            self.Data=source #数据源
+            self.DataT=target
+            self.Amount=len(target)  #分析的个数
+            self.V1=target['close'].values #分析的数据集
+            self.V2=source['close'].values #总数据集
+            self.V2_LEN=len(self.V2)
+        print("__init__")
+#         self.FIG,self.AX = plt.subplots(figsize=(30,12),dpi=200)
+#         self.FIG=plt.figure(figsize=(30,12),dpi=200)
+#         self.AX=self.FIG.add_subplot(111)
         
     def corr(self):
         resList=[]
@@ -58,9 +63,13 @@ class Analyse_CORR:
     
     #draw kline
     def DrawKK(self,aa,add1=0,add2=0,preF='T_',corr=0,isSave=False):
-        #fig = plt.figure(figsize=(30, 12))
-        ax = self.fig.add_subplot(111)
-        ax.clear()
+        aa.reset_index(drop=True,inplace=True)
+        fig =plt.figure(figsize=(30, 12),facecolor='w')
+        ax =fig.add_subplot(111)       
+#         ax = AA.Axes(self.FIG, [0, 0, 0.9, 0.9])  #用[left, bottom, weight, height]的方式定义axes，0 <= l,b,w,h <= 1
+#         self.FIG.add_axes(ax)
+#         ax.clear()
+#         ax.clear()
         k_width = 50
         k_diff = 10
         kline = k_width + k_diff
@@ -69,7 +78,7 @@ class Analyse_CORR:
         if add1>0:
             ax.axvline((add1+1)*kline,ymax=1,c='b',ls='dashed')
         if add2>0:
-            ax.axvline((rows-add2+1)*kline,ymax=1,c='b',ls='dashed')
+            ax.axvline((add1+self.Amount+1)*kline,ymax=1,c='b',ls='dashed')
         for i in aa.iterrows():
             d=d+1
             x = d * kline
@@ -101,20 +110,33 @@ class Analyse_CORR:
             # K线实体
             rec = Rectangle((x, y), k_width, hh, fill=a, color=cc)
             ax.add_patch(rec)
+            
+        plt.plot(aa.index*kline + k_width / 2,aa['ma60'],c='r')
+        plt.plot(aa.index*kline + k_width / 2,aa['ma30'],c='b')
         ax.grid()
+        ax.autoscale(tight=False)
         ax.autoscale_view()
         time1=aa.iloc[add1]['datetime']
         if add2==0:
             diff=-1
         else:
-            diff=-add2-1
+            diff=add1+self.Amount          
+        if (diff-1)>rows:
+            diff=rows-1
+               
         time2=aa.iloc[diff]['datetime']
         title="%s--%s,Corr:%.3f" %(time1,time2,corr)
-        plt.title(title, fontsize=20, color='r')
+        ax.set_title(title, fontsize=20, color='r')
         if isSave:
-            file="res/%s%d_%.7s.jpg"%(PreF,self.Amount,time1)
+            file="res/%s_%.7s.jpg"%(preF,time1)
             fig.savefig(file)
-        return plt
+            #self.FIG=fig
+            fig.clear()
+            plt.close(fig)
+        else:
+            plt.show()
+            fig.clear()
+            plt.close(fig)
     
     def DrawIndex(self,ind,add1=0,add2=0,isSave=False,preF='T_'):
         if add1>ind:
@@ -123,12 +145,14 @@ class Analyse_CORR:
         length=self.Amount
         end=begin+length+add2+add1
         df2=self.Data.iloc[begin:end]
+        df2.reset_index(inplace=True)
+        self.D2=df2
         v1=self.DF_CORR.loc[ind]['Corr']
         file="res/%s.jpg"%(ind)
-        self.DrawKK(df2,add1=add1,add2=add2,corr=v1,isSave=isSave,pref=preF)
+        self.DrawKK(df2,add1=add1,add2=add2,corr=v1,isSave=isSave,preF=preF)
         
     def DrawNo(self,no,add1=0,add2=0,isSave=False,preF='T_'):
         index=self.DF_CORR.index[no]
-        self.DrawIndex(index,add1,add2,isSave,preF=preF)
+        self.DrawIndex(index,add1,add2,isSave,preF=preF+str(no)+"_")
         
 print("OK")
