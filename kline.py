9a0d74b5203c58800cf7a6af8e6d85d2c5b1ed65
che7611 <39072982@qq.com>
2019-05-27 01:37:22 +0800

    Merge branch 'master' of https://github.com/che7611/test2
    # Conflicts:
    #	.ipynb_checkpoints/HL_Strategy-checkpoint.ipynb
    #	HL_Strategy.ipynb

--- kline.py
+++ kline.py
@@ -7,13 +7,14 @@ from matplotlib.patches import Rectangle
 import matplotlib as mpl
 import matplotlib.dates as mdate
 from datetime import time
+from typing import Iterable
 
 class Kline(object):
-    def DrawKline(self,aa):
+    def DrawKline(self,aa,**para):
     #     date='2014-01-03'
         # aa=df1[str(date)]
         aa.reset_index(drop=True,inplace=True)
-        fig =plt.figure(figsize=(30, 12),facecolor='w')
+        fig =plt.figure(figsize=(45, 18),facecolor='w')
 
         #副图是主图的比例
         w1=0.25
@@ -36,6 +37,64 @@ class Kline(object):
         Kline['W'] = 80
         Kline['Diff'] = 20
         Kline['WW']=100
+        
+        if 'rec' in para.keys():
+            id1=para['rec'][0]
+            id2=para['rec'][1]
+            y1=aa[id1:id2]['close'].max()
+            y2=aa[id1:id2]['close'].min()
+            a1 =(id1+1)*Kline['WW']
+            a2 =(id2+1)*Kline['WW']
+            ax.axvline(a1,ymax=1,c= "b",ls = "--",linewidth=1)    
+            ax.axvline(a2,ymax=1,c= "b",ls = "--",linewidth=1)
+            ax.axhline(y1,xmax=1,c= "b",ls = "--",linewidth=1)
+            ax.axhline(y2,xmax=1,c= "b",ls = "--",linewidth=1)
+        
+        if 'buy' in para.keys():
+            buy=para['buy']
+            if not isinstance(buy, Iterable):
+                buy = [buy]
+            for pos in buy:
+                x =(pos+1)*Kline['WW']
+                y=aa.iloc[pos]['low']-5
+                ax.plot(x,y,'r^')
+        
+        if 'sell' in para.keys():
+            sell=para['sell']
+            if not isinstance(sell, Iterable):
+                sell = [sell]
+            for pos in sell:
+                x =(pos+1)*Kline['WW']
+                y=aa.iloc[pos]['high']+5
+                ax.plot(x,y,'gv')
+        
+        if 'trade' in para.keys():
+            trade=para['trade']
+            if trade[0]=='buy':
+                x1 =(trade[1]+1)*Kline['WW']
+                y1=aa.iloc[trade[1]]['low']-5
+                x2 =(trade[2]+1)*Kline['WW']
+                y2 =aa.iloc[trade[2]]['high']+5
+                ax.plot(x1,y1,'r^')
+                ax.plot(x2,y2,'gv')
+                y3=aa.iloc[trade[1]]['close']
+                y4=aa.iloc[trade[2]]['close']
+                ax.plot([x1,x2],[y3,y4],c='m',ls='--',linewidth=2)
+                txt="Buy Diff:%s" %(y4-y3)
+                ax.text(x2,y2+10,txt,fontsize=14,color='r')
+            elif trade[0]=='sell':
+                x1 =(trade[1]+1)*Kline['WW']
+                y1=aa.iloc[trade[1]]['high']+5
+                x2 =(trade[2]+1)*Kline['WW']
+                y2 =aa.iloc[trade[2]]['low']-5
+                ax.plot(x1,y1,'gv')
+                ax.plot(x2,y2,'r^')
+                y3=aa.iloc[trade[1]]['close']
+                y4=aa.iloc[trade[2]]['close']
+                ax.plot([x1,x2],[y3,y4],c='g',ls='--',linewidth=2)
+                txt="Sell Diff:%s" %(y3-y4)
+                y2 =aa.iloc[trade[2]]['high']+10
+                ax.text(x2,y2,txt,fontsize=14,color='r')
         d=0
         rows=len(aa)
         for i in aa.iterrows():
@@ -77,9 +136,10 @@ class Kline(object):
                 rec= Rectangle((x2, 0), Kline['W'], Row['macd'], fill=a, color='C')
             ax1.add_patch(rec)
             d+=1
-        if "ma60" in aa.columns:
+            
+        if any(["ma60" in aa.columns,"MA60" in aa.columns]):
             ax.plot(aa.index*Kline['WW']+Kline['WW'],aa['ma60'],c='r')
-        if "ma30" in aa.columns:
+        if any(["ma30" in aa.columns,"MA30" in aa.columns]):
             ax.plot(aa.index*Kline['WW']+Kline['WW'],aa['ma30'],c='b')
         ax1.axhline(0,linestyle="--",linewidth=0.5,color='k')
         if "diff" in aa.columns:
@@ -96,8 +156,13 @@ class Kline(object):
         time2=aa.iloc[-1]['datetime']
         title="%s--%s" %(time1,time2)
         ax.set_title(title, fontsize=20, color='r')
-        # fig.savefig("a.jpg")
-        plt.show()
+        
+        if "save" in para.keys():
+            file=para['save']
+            fig.savefig(file)
+            print(file)
+        else:
+            plt.show()
         fig.clear()
         plt.close(fig)
 
